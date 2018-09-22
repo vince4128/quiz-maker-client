c922f1ea896f4db6e260ad208e984b1c38085d7f
vince4128 <vrlt.vincent@gmail.com>
2018-09-22 19:23:27 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Quiz Maker</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to QuizMaker.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
