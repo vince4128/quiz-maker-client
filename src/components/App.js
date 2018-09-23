b11651e00fbbbe729ff44e31e14b2d6fbe53df00
vince4128 <vrlt.vincent@gmail.com>
2018-09-23 21:53:50 +0200

    Routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Header from './header/Header';
 import QuizIndex from './quiz/Quiz.index';
 import QuizShow from './quiz/Quiz.show';
+import QuizCreate from './quiz/Quiz.create';
+import CategoryIndex from './category/category.index';
+import CategoryShow from './category/category.show';
+//import UserIndex from './user/user.index';
+//import UserShow from './user/user.show';
+import Signin from './auth/Signin';
+import Signup from './auth/Signup';
+import Signout from './auth/Signout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Quiz Maker</h1>
-        </header>
-        <Route path="/" exact component={QuizIndex} />
-        <Route path="/:id" exact component={QuizShow} />
+
+        <Header auth={this.props.auth}/>
+
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signout" exact component={Signout} />
+
+        <Route path="/" exact render={()=><QuizIndex connected={this.props.auth}/>} />
+
+        <Switch>
+          <Route path="/quiz/new" render={()=><QuizCreate connected={this.props.auth}/>} />
+          <Route path="/quiz/:id" render={()=><QuizShow connected={this.props.auth}/>} />
+        </Switch>
+
+        <Route path="/category" exact render={()=><CategoryIndex connected={this.props.auth}/>} />
+
+        <Switch>
+          <Route path="/category/:id" render={()=><CategoryShow connected={this.props.auth}/>} />
+        </Switch>   
+
         <p className="App-intro">
           Welcome to QuizMaker.
         </p>
@@ -21,4 +45,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return { auth:state.auth, _id:state.auth._id}
+}
+
+export default connect(mapStateToProps)(App);
