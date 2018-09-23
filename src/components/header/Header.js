5b70d05aa9600928d7d755790783e12cc2769488
vince4128 <vrlt.vincent@gmail.com>
2018-09-23 21:53:36 +0200

    header added

---
+++ src/components/header/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Route, Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+
+    renderLinks(){
+        if(this.props.auth.authenticated) {
+            return(
+                <span>
+                    <li><Link to={"/category"}>Categories</Link></li>
+                    { this.props.location.pathname === "/" ? <li><Link to={"/quiz/new"}>New Quiz</Link></li> : "" }                    
+                    <li><Link to={"/signout"}>Sign out</Link></li>
+                </span>
+            )
+        }else{
+            return(
+                <div>
+                    <li><Link to={"/signin"}>Sign in</Link></li>
+                    <li><Link to={"/signup"}>Sign up</Link></li>
+                </div>
+            )
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <header className="App-header">
+                    <h1 className="App-title">Quiz Maker</h1>
+                </header>
+                <ul>
+                    <li><Link to={"/"}>Home</Link></li>
+                    {this.renderLinks()}
+                </ul>              
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(Header);
