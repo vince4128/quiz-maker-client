115921464a349a877193c093103ae728add24307
vince4128 <vrlt.vincent@gmail.com>
2018-09-24 17:54:26 +0200

    category actions added

--- src/actions/category.action.js
+++ src/actions/category.action.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import {
     FETCH_CATEGORIES,
-    FETCH_CATEGORY
+    FETCH_CATEGORY,
+    CREATE_CATEGORY,
+    EDIT_CATEGORY,
+    DELETE_CATEGORY
 } from './types';
 
 const server = 'http://localhost:3000';
@@ -21,5 +24,41 @@ export function fetchCategory(id){
     return {
         type:FETCH_CATEGORY,
         payload:response
+    }
+}
+
+export function createCategory(values, token, callback){
+    const request = axios.post(`${server}/category`,values, {
+        headers: {authorization: token}
+    })
+        .then(() => callback());
+
+    return {
+        type: CREATE_CATEGORY,
+        payload: request
+    }
+}
+
+export function editCategory(id, values, token, callback){
+    const request = axios.put(`${server}/category/${id}`,values, {
+        headers: {authorization:token}
+    })
+        .then(() => callback());
+
+    return {
+        type: EDIT_CATEGORY,
+        payload: request
+    }
+}
+
+export function deleteCategory(id, token){
+    
+    axios.delete(`${server}/category/${id}`, {
+        headers: {authorization:token}
+    });
+
+    return {
+        type: DELETE_CATEGORY,
+        payload: id
     }
 }
