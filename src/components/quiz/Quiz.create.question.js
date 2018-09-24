fdbde18d0054594c51cf2704ddc1c9473f5a0967
vince4128 <vrlt.vincent@gmail.com>
2018-09-24 17:55:42 +0200

    Quiz create question and Quiz edit added

---
+++ src/components/quiz/Quiz.create.question.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { Route, Link, withRouter } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { fetchQuiz } from '../../actions';
+import requireAuth from '../requireAuth';
+import RenderField from '../field/RenderField';
+import RenderSelectField from '../field/RenderSelectField';
+import QuestionIndex from '../question/Question.index';
+import QuestionCreate from '../question/Question.create';
+
+class QuizCreateQuestion extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedQuiz:null          
+        }
+    }
+
+    componentDidMount(){
+        const { id } = this.props.match.params;
+        this.setState({selectedQuiz:id});
+        this.props.fetchQuiz(id);
+    }
+
+    renderQuiz(){
+
+        if(this.props.quiz[`${this.state.selectedQuiz}`]){
+
+            const quiz = this.props.quiz[`${this.state.selectedQuiz}`];
+
+            return Object.keys(quiz)
+                .map((q) => {
+                    if(typeof quiz[q] === "string"){return <li key={q}>{q} : {quiz[q]}</li>}
+                    //return typeof q;
+                })
+
+        }else{
+            return "loading";
+        }
+
+        
+    }
+
+    render(){
+
+        const data = Object.assign({}, this.props.quiz);
+        let Quiz = {};
+        data[this.state.selectedQuiz] ? Quiz = data[this.state.selectedQuiz] : Quiz = {err:'objet inexistant'};
+
+        const hasQuiz = 
+            <diV>
+            <h2>Quiz Create Question</h2>
+            <h3>Le quiz</h3>
+            <ul>{this.renderQuiz()}</ul>
+            <hr/>
+            { Quiz.question ? <QuestionIndex questions={Quiz.question}/> : "" }            
+            <p>Ajout de question</p>            
+            {
+                /*Ajouter un bouton enregistrer qui : */
+                /*Appelle une action (à créer) qui prend en parametre l'id du quiz et push la question dedans*/
+                /*Appeler l'action editQuiz avec le quiz présent dans le reducer*/
+                /*Ou utiliser action existante editQuiz...*/
+            }
+        </diV>
+
+        const noQuiz = <p>Loading</p>;
+
+        return this.props.quiz[`${this.state.selectedQuiz}`] ? hasQuiz : noQuiz;
+
+    }
+
+}
+
+function mapStateToProps(state){
+    return { quiz:state.quiz};
+}
+
+export default withRouter(requireAuth(connect(mapStateToProps,{ fetchQuiz })(QuizCreateQuestion)));
---
+++ src/components/quiz/Quiz.edit.js
@@ -0,0 +1,174 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { withRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { editQuiz, fetchQuiz, fetchCategories, fetchImages } from '../../actions';
+import requireAuth from '../requireAuth';
+import RenderField from '../field/RenderField';
+import RenderSelectField from '../field/RenderSelectField';
+
+class QuizEdit extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedQuiz: null,
+        }
+    }
+
+    componentDidMount(){
+        const { id } = this.props.match.params;
+        this.setState({selectedQuiz:id});
+        this.props.fetchQuiz(id);
+        this.props.fetchCategories();
+        this.props.fetchImages();
+    }
+
+    renderCategories(){
+        //avoid mutate
+        const data = Object.assign({}, this.props.categories);
+
+        //options to return
+        return Object.keys(data).map(            
+            (cat)=>{
+                return(
+                    <option key={data[cat]._id} value={data[cat]._id}>{data[cat].title}</option>
+                )
+            }
+        );    
+    }
+
+    renderImages(){
+        //avoid mutate
+        const data = Object.assign({}, this.props.images);
+        //options to return
+        return Object.keys(data).map(            
+            (img)=>{
+                return(
+                    <option key={data[img]._id} value={data[img]._id}>{data[img].title}</option>
+                )
+            }
+        );    
+    }
+
+    renderTagsField(field){
+        return(
+            <div className="form-group">
+
+            </div>
+        );
+    }
+
+    onSubmit(values){
+        this.props.editQuiz(this.state.selectedQuiz, values, this.props.connected.authenticated ,() => {
+            this.props.history.push('/');
+        });
+    }
+
+    render(){
+
+        const { handleSubmit } = this.props;
+
+        return(
+
+            <div>
+
+            <h1>Edit Quiz</h1>
+
+            <hr/>
+
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                
+                <Field
+                    label="Title"
+                    name="title"
+                    placeholder="Type your title"
+                    defaultValue ="blablalba"            
+                    component={RenderField}                    
+                />
+
+                <Field
+                    label="Description"
+                    name="description"
+                    placeholder="placeholder"
+                    placeholder="Type your title"                                        
+                    component={RenderField}
+                />
+
+                <Field
+                    label="Short Description"
+                    name="shortdescription"
+                    placeholder="placeholder"
+                    placeholder="Type your title"                                        
+                    component={RenderField}
+                />
+
+                {/*<Field
+                    label="Image"
+                    name="image"
+                    component={RenderSelectField}>
+                    {this.renderImages()}
+                </Field>*/}
+
+                <Field
+                    label="Category"
+                    name="category"
+                    component={RenderSelectField}>
+                    {this.renderCategories()}
+                </Field>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+
+            </form>
+
+        </div>
+        )
+
+    }
+    
+}
+
+function validate(values){
+
+    const errors = {};
+
+    // validate the inputs from 'values'
+    if(!values.title){
+        errors.title = "Enter a title !";
+    }
+
+    if(!values.description){
+        errors.description = "Enter a description !";
+    }
+
+    if(!values.shortdescription){
+        errors.shortdescription = "Enter a shortdescription !";
+    }
+
+    if(!values.category){
+        errors.category = "Choose a category !";
+    }
+
+    {/*if(!values.image){
+        errors.image = "Choose a image !";
+    }*/}
+
+    //if errors is empty, the form is fine to submit
+    //if errors as any property, redux form is invalid
+    return errors;
+
+}
+
+function mapStateToProps(state){
+    return { quiz:state.quiz, categories:state.categories, images:state.images };
+}
+
+export default reduxForm({
+    validate:validate,
+    initialValues:{ field1: 'value1', field2: 'value2' }, 
+    form:'EditQuizForm'   //name must be unique (in case of several form it's usefull), and could be whatever string we want. 
+})(
+    withRouter(requireAuth(connect(mapStateToProps, { editQuiz, fetchQuiz, fetchCategories, fetchImages })(QuizEdit)))
+);
