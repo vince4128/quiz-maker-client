d080d0ac28326deef575f58f6da9063f1c7ff793
vince4128 <vrlt.vincent@gmail.com>
2018-09-26 17:45:40 +0200

    method add for test with questions.edit

--- src/components/question/Question.index.js
+++ src/components/question/Question.index.js
@@ -9,7 +9,13 @@ const QuestionIndex = (props) => {
             return props.questions.map((q) => {
                 return (
                     <div key={q._id}>
-                        <QuestionShow question={q} edit={props.edit}/>
+                        <QuestionShow 
+                        question={q} 
+                        quizId={props.quizId} 
+                        edit={props.edit} 
+                        connected={props.connected}
+                        deleteMethod={(qid)=>props.deleteMethod(qid)}
+                        parentMethod={()=>props.parentMethod()}/>
                     </div>
                 )
             })
@@ -22,7 +28,7 @@ const QuestionIndex = (props) => {
         <div>
             <h4>Question index</h4>
             {renderQ()}
-            <Link to={`/quiz/${props.quizId}/question/new`}>Edit/Add Questions</Link>            
+            <Link to={`/quiz/${props.quizId}/question/new`}>Add Questions</Link>            
             <hr/>
         </div>        
     )
