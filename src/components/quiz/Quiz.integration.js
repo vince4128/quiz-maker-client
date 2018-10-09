4a58d04cd08d2ea5d04148139a0376df5a8e092d
vince4128 <vrlt.vincent@gmail.com>
2018-10-09 23:55:38 +0200

    copy to clipboard

--- src/components/quiz/Quiz.integration.js
+++ src/components/quiz/Quiz.integration.js
@@ -7,11 +7,19 @@ class QuizIntegration extends Component{
         super(props);
     }
 
-    linkToClipboard = (e) => {        
-        this.linkref.select();
-        document.execCommand('copy');
-        e.target.focus();
-        alert('lien copié dans le presse-papier');        
+    linkToClipboard = (e,type) => {        
+        if(type==='link'){
+            this.linkref.select();
+            document.execCommand('copy');
+            e.target.focus();
+            alert('lien copié dans le presse-papier');      
+        }else{
+            this.textarearef.select();
+            document.execCommand('copy');
+            e.target.focus();
+            alert('code embed copié dans le presse papier');
+        }
+          
     }
 
     render(){
@@ -22,17 +30,16 @@ class QuizIntegration extends Component{
                 <section>
                     <h3>Via une url</h3>                
                     <input ref={(linkref) => this.linkref = linkref} type="text" value={`http://localhost:3001/shared/${this.props.match.params.id}`} id="sharedQuiz"/>
-                    <button onClick={this.linkToClipboard}>Copier le lien</button>
+                    <button onClick={(e)=>this.linkToClipboard(e,'link')}>Copier le lien</button>
                 </section>
                 <section>
                     <h3>Via un code d'intégration</h3>
                     <input 
-                        type="textarea" 
-                        value={`http://localhost:3001/shared/${this.props.match.params.id}`}
-                        value={`<iframe src="http://localhost:3001/shared/${this.props.match.params.id}" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" width="100%" height="100vh"></iframe>`} 
-                        
+                        type="textarea"
+                        ref={(textarearef) => this.textarearef = textarearef} 
+                        value={`<iframe src="http://localhost:3001/shared/${this.props.match.params.id}" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" width="100%" height="100vh"></iframe>`}                         
                         id="iframeSharedQuiz"/>
-                <button onClick={this.linkToClipboard}>Copier le code</button>
+                <button onClick={(e)=>this.linkToClipboard(e,'embed')}>Copier le code</button>
                 </section>
             </div>
         )
