da48052f5b9bc5bc0f7f298b723338a035b34cc1
vince4128 <vrlt.vincent@gmail.com>
2018-10-17 17:49:38 +0200

    class added

--- src/components/quiz/Quiz.integration.js
+++ src/components/quiz/Quiz.integration.js
@@ -27,19 +27,19 @@ class QuizIntegration extends Component{
             <div className="o-content">
                 <h1>Code d'intégration du quiz</h1>
                 <hr/>
-                <section>
+                <section className="m-form">
                     <h3>Via une url</h3>                
                     <input ref={(linkref) => this.linkref = linkref} type="text" value={`http://localhost:3001/shared/${this.props.match.params.id}`} id="sharedQuiz"/>
-                    <button onClick={(e)=>this.linkToClipboard(e,'link')}>Copier le lien</button>
+                    <button className="m-button m-button--primary" onClick={(e)=>this.linkToClipboard(e,'link')}>Copier le lien</button>
                 </section>
-                <section>
+                <section className="m-form">
                     <h3>Via un code d'intégration</h3>
                     <input 
                         type="textarea"
                         ref={(textarearef) => this.textarearef = textarearef} 
                         value={`<iframe src="http://localhost:3001/shared/${this.props.match.params.id}" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" width="100%" height="100vh"></iframe>`}                         
                         id="iframeSharedQuiz"/>
-                <button onClick={(e)=>this.linkToClipboard(e,'embed')}>Copier le code</button>
+                    <button className="m-button m-button--primary" onClick={(e)=>this.linkToClipboard(e,'embed')}>Copier le code</button>
                 </section>
             </div>
         )
