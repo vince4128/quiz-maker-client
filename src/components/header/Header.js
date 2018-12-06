497d1b293d872d233bf7792369ad08d7ac462371
vince4128 <vrlt.vincent@gmail.com>
2018-12-06 17:15:34 +0100

    header in preview mode

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -32,10 +32,23 @@ class Header extends Component {
     }
 
     render(){
+
+        const previewMode = this.props.location.pathname.includes('preview')
+
         return(
             <header className="o-header header-container">                
                 <ul>
-                    <li onClick={()=>{this.toggleClassBody()}}>
+                    { previewMode ? 
+                        <li>
+                            <Link to="/" className="o-header__back">
+                                <i>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                                    <path fill="#fff" d="M0.646 10.146l6-6c0.195-0.195 0.512-0.195 0.707 0s0.195 0.512 0 0.707l-5.146 5.146h16.293c0.276 0 0.5 0.224 0.5 0.5s-0.224 0.5-0.5 0.5h-16.293l5.146 5.146c0.195 0.195 0.195 0.512 0 0.707-0.098 0.098-0.226 0.146-0.354 0.146s-0.256-0.049-0.354-0.146l-6-6c-0.195-0.195-0.195-0.512 0-0.707z"></path>
+                                    </svg>
+                                </i>
+                            </Link>
+                        </li>
+                    : <li onClick={()=>{this.toggleClassBody()}}>
                         <a className="o-header__trigger" href="#">
                             <i>
                                 <svg className="test" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
@@ -50,8 +63,8 @@ class Header extends Component {
                                 </svg>
                             </i>
                         </a>
-                    </li>
-                    <li><h3 id="app-title">Quiz Maker</h3></li>
+                    </li>}
+                    <li><h3 id={"app-title"} className={previewMode ? "app-title--preview" : ""}>Quiz Maker</h3></li>
                     {this.renderLinks()}
                 </ul>    
             </header>                          
