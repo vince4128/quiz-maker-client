d7d8cfde0039cfa6f8edada2696a09bf39beb49b
vince4128 <vrlt.vincent@gmail.com>
2019-02-10 22:44:39 +0100

    url changed

--- src/actions/server.js
+++ src/actions/server.js
@@ -1,4 +1,4 @@
-const SERVER = 'http://localhost:3000';
-//const SERVER = 'http://138.197.186.223:3000';
+//const SERVER = 'http://localhost:3000';
+const SERVER = 'http://138.197.186.223:3000';
 
 export default SERVER;
