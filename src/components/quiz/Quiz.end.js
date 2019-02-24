30af6155b9126865642a82d76c409d247ea1ba24
vince4128 <vrlt.vincent@gmail.com>
2019-02-24 15:36:56 +0100

    feedback class added

--- src/components/quiz/Quiz.end.js
+++ src/components/quiz/Quiz.end.js
@@ -5,14 +5,14 @@ import Score from '../ui/score';
 const QuizEnd = (props) => {
 
     return(
-        <div>
-            <h2 className="a-question--score">Votre score est de : {Math.round(props.scorePercent)} %</h2>
+        <div>            
             <div className="a-graph-result">
                 <Score score={props.scorePercent}></Score>
-            </div>                                    
+            </div>
+            {/*<h2 className="a-question--score">Votre score est de : {Math.round(props.scorePercent)}%</h2>*/}
             {props.scorePercent > 80 ? 
-                <h4 dangerouslySetInnerHTML={{__html: props.feedback.good}}/>
-                : <h4 dangerouslySetInnerHTML={{__html: props.feedback.bad}}/>
+                <h4 className="m-quiz__feedback" dangerouslySetInnerHTML={{__html: props.feedback.good}}/>
+                : <h4 className="m-quiz__feedback" dangerouslySetInnerHTML={{__html: props.feedback.bad}}/>
             }            
         </div>        
     )
